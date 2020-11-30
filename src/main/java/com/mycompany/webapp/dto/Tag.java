847651c11b6b5ea809eb2fe47035579bb32ce23b
COM <COM@DESKTOP-L2F7LSG>
2020-11-30 23:01:22 +0900

    Implement tag function

---
+++ src/main/java/com/mycompany/webapp/dto/Tag.java
@@ -0,0 +1,19 @@
+package com.mycompany.webapp.dto;
+
+public class Tag {
+	private String tname;
+	private int bno;
+	
+	public String getTname() {
+		return tname;
+	}
+	public void setTname(String tname) {
+		this.tname = tname;
+	}
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+}
